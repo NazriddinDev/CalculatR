3cdf7f8351097e4b81d98afbf65d3687ec806c57
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-05-15 19:02:33 +0500

    INFRA=> Upload Project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.Write("Birinchi raqamni kiriting => ");
+int birinchiRaqam= Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Ikkinchi raqamni kiriting => ");
+int ikkinchiRaqam= Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Ikki sonning yig'indisi {birinchiRaqam+ikkinchiRaqam}");
+Console.WriteLine($"Ikki sonning ayirmasi {birinchiRaqam-ikkinchiRaqam}");
+Console.WriteLine($"Ikki sonning ko'paytmasi {birinchiRaqam*ikkinchiRaqam}");
+Console.WriteLine($"Ikki sonning qoldiqsiz bo'linmasi {birinchiRaqam/ikkinchiRaqam}");
+Console.WriteLine($"Ikki sonning qoldiqli bo'linmasi {birinchiRaqam%ikkinchiRaqam}");
