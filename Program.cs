9de085ca8d9d74b8b1555b62176060ed0edd6752
qurbonovnazriddin@gmail.com <qurbonovnazriddin@gmail.com>
2024-05-19 09:01:33 +0500

    TEACH: Use if confition

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿System.Console.Write("Birinchi raqamni kiriting => ");
+﻿using System;
+
+Console.Write("Birinchi raqamni kiriting => ");
 int birinchiRaqam= Convert.ToInt32(Console.ReadLine());
-System.Console.Write("Ikkinchi raqamni kiriting => ");
+Console.Write("Ikkinchi raqamni kiriting => ");
 int ikkinchiRaqam= Convert.ToInt32(Console.ReadLine());
+
+if(birinchiRaqam>ikkinchiRaqam)
+{
+    Console.WriteLine("Birinchi son ikkinchisidan katta...");
+}
+else if(birinchiRaqam==ikkinchiRaqam)
+{
+    Console.WriteLine("Ikkala son o'zaro teng...");
+}
+else
+{
+    Console.WriteLine("Birinchi son ikkinchisidan kichik...");
+}
 
 Console.WriteLine($"Ikki sonning yig'indisi {birinchiRaqam+ikkinchiRaqam}");
 Console.WriteLine($"Ikki sonning ayirmasi {birinchiRaqam-ikkinchiRaqam}");
 Console.WriteLine($"Ikki sonning ko'paytmasi {birinchiRaqam*ikkinchiRaqam}");
 Console.WriteLine($"Ikki sonning qoldiqsiz bo'linmasi {birinchiRaqam/ikkinchiRaqam}");
 Console.WriteLine($"Ikki sonning qoldiqli bo'linmasi {birinchiRaqam%ikkinchiRaqam}");
+
+// Console.WriteLine($"Birinchi raqam ikkinchi raqamdan katta {birinchiRaqam > ikkinchiRaqam}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqamdan katta yoki teng {birinchiRaqam >= ikkinchiRaqam}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqamdan kichik {birinchiRaqam > ikkinchiRaqam}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqamdan kichik yoki teng {birinchiRaqam <= ikkinchiRaqam}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqam teng {birinchiRaqam == ikkinchiRaqam}");
+// Console.WriteLine($"Birinchi raqam ikkinchi raqam teng emas {birinchiRaqam != ikkinchiRaqam}");
